a781594577c46762a3081c105082276a8bc0f060
GoncharEvgeniy <uhp.gonchar.evgeniy@gmail.com>
2021-01-23 20:41:32 +0200

    Create database

--- fitness-server/src/main/java/com/fitness/repository/RoleRepo.java
+++ fitness-server/src/main/java/com/fitness/repository/RoleRepo.java
@@ -3,5 +3,5 @@ package com.fitness.repository;
 import com.fitness.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepo extends JpaRepository<Exercise, Long> {
+public interface RoleRepo {
 }
